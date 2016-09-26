f2158d82588be07060127f1e3c861e8179b0deba
Anders Spælling <anders.spaelling@lumagate.com>
2016-09-26 13:05:13 +0200

    test

---
+++ test.ps1
@@ -0,0 +1,4 @@
+﻿
+$WebhookURI = "https://webhook-logger.azurewebsites.net/api/HelloAzureFunctions?code=fkeq1mheeo42a5c6nf6rpmn29yyzq7xqjqb7wt10gdhj9py14isdkdlk54xx93cd0tvlo8khuxr"
+$postParams = @{name='codebeaver.blogspot.com';}
+Invoke-WebRequest -Uri $WebhookURI -Method GET -Body $postParams | Select-Object -ExpandProperty Content
